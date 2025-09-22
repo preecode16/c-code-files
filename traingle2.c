7b9f4d45b7c6729ee70dba1415f10560685bc2e4
preetham <preethamrgatty370@gmail.com>
2025-09-22 19:09:50 +0530

    changed # into *

--- traingle2.c
+++ traingle2.c
@@ -1,30 +1,28 @@
 #include<stdio.h>
 
 int main(){
-
     int n;
 
     printf("enter the size of pyramid:");
     scanf("%d" , &n);
 
-    if(n <= 0)
+    if(n < 0)
     {
-        printf("enter a postive number:\n");
-    
-return 1;
+        printf("enter a postive digit\n");
+        return 1;
+
     }
 
     for(int i = 0; i < n; i++)
     {
-    
-        for(int j =  0; j < n - i - 1; j++)
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            printf(" ");
 
-        {
-        printf(" ");
         }
-for(int k = 0; k < 2 * i + 1; k++)
+        for(int k = 0; k < 2 * i + 1; k++)
         {
-        printf("#");
+            printf("*");
         }
         printf("\n");
     }
