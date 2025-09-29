13014899c52b3110c458b24bd3c9db10fb4c9f4e
preetham <preethamrgatty370@gmail.com>
2025-09-29 07:07:06 +0530

    phonebook

--- phonebook.c
+++ phonebook.c
@@ -1,24 +1,25 @@
 #include<stdio.h>
-const n = 3;
+#include<string.h>
+
 
 int main(){
-    char *name[] = {"preetham" , "vani" , "raghavendra" };
-    char *number[] = {"+91-808-862-9562" , "+91-984-424-5083" , "+91-994-599-6929"};
+    char name[3][20] = {"preetham" , "vani" , "raghavendra" };
+    char number[3][20] = {"+91-808-862-9562" , "+91-984-424-5083" , "+91-994-599-6929"};
 
     char names[20];
 
     printf("enter your name:");
-    scanf("%s" , name);
+    scanf("%s" , names);
 
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < 3; i++)
     {
         if(strcmp(name[i] , names) == 0)
         
         {
-            printf("found %d\n" , number[i]);
+            printf("found %s\n" , number[i]);
             return 0;
         }
     }
-    print("not found\n");
+    printf("not found\n");
     return 1;
 }
