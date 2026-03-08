2b2bfa9799c13e49eaf3a571a604bfadbbadd85b
preetham <preethamrgatty370@gmail.com>
2026-03-08 11:58:24 +0530

    checks whether number is odd or even

--- odd_function.c
+++ odd_function.c
@@ -1,40 +1,42 @@
 #include<stdio.h>
+// function prototype
 void separate_odd_even(int*n ,int numbers[] , int odd[] , int even[] , int*odd_count , int*even_count);
 int main(){
     int n;
     
-    int odd_count = 0 , even_count = 0;
+    int odd_count = 0 , even_count = 0; // to check how many odd  and even numbers
 
-    printf("enter how many numbers you want to enter:");
+    printf("enter how many numbers you want to enter:"); // asking user input data
     scanf("%d" , &n);
 
     int numbers[n];
-    int odd[n];
+    int odd[n];      // intilaziation of odd , even and numbers array
     int even[n];
     
 
     for ( int i = 0; i < n; i++)
     {
-        printf("enter number %d:" , i + 1);
+        printf("enter number %d:" , i + 1); // input numbers from user
     scanf("%d" , &numbers[i]);
     }
     
 
-    separate_odd_even(&n ,numbers , odd , even , &odd_count , &even_count);
+    separate_odd_even(&n ,numbers , odd , even , &odd_count , &even_count); // function call
 
     printf("the odd numbers are:\n");
     for(int i = 0; i < odd_count; i++)
-    {
+    {                                  // prints the odd numbers
         printf(" %d" ,odd[i]);
     }
 
     printf("\nthe even numbers are:\n");
     for(int i = 0; i < even_count; i++)
-    {
+    {                                   // prints the even number
         printf(" %d" ,even[i]);
     }
     return 0;
 }
+//function defenation
 void separate_odd_even(int*n ,int numbers[] , int odd[] , int even[] , int*odd_count , int*even_count)
 {
 
